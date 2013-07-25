d75e427fbab28007cf78d218735f82ee437deac8
David Bankier <dbankier@hotmail.com>
2013-07-25 10:47:16 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# grunt-ltss
+
+> grunt plugin for ltss
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-ltss --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-ltss');
+```
+
+## The "ltss" task
+
+### Overview
+In your project's Gruntfile, add a section named `ltss` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  ltss: {
+    options: {
+      // Doesn't really apply to this grunt plugin yet
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
+```
+
+### Options
+
+### Usage Examples
+
+
+```js
+grunt.initConfig({
+  ltss: {
+    options: {},
+    files: {
+      'dest/view.tss': ['src/view.ltss'],
+    },
+  },
+})
+```
---
+++ tasks/ltss.js
@@ -0,0 +1,52 @@
+/*
+ * grunt-ltss
+ * None
+ *
+ * Copyright (c) 2013 David Bankier
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var ltss = require("ltss");
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('ltss', 'Your task description goes here.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '',
+      separator: ', '
+    });
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+      ltss.compileString(src, function(err, data) {
+        // Write the destination file.
+        grunt.file.write(f.dest, data);
+
+        // Print a success message.
+        grunt.log.writeln('File "' + f.dest + '" created.');
+      });
+    });
+  });
+
+};
